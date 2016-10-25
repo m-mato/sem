c1ba1b36dba9fc3e7e385b4738863788d0fd0177
kamil-triscik <kamil28193@gmail.com>
2016-10-25 16:30:59 +0200

    Sport entity added

---
+++ src/main/java/cz/muni/pa165/sem/entity/Sport.java
@@ -0,0 +1,84 @@
+package cz.muni.pa165.sem.entity;
+
+import com.sun.istack.internal.NotNull;
+
+import javax.persistence.*;
+
+/**
+ *
+ *
+ * @author Kamil
+ */
+@Entity
+public class Sport {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Column(name = "name")
+    private String name;
+
+    @NotNull
+    @Column(name = "description")
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 53;
+        int hashCode = 7;
+        hashCode = 43 * hashCode + ((this.name == null) ? 0 : this.name.hashCode());
+        hashCode = 43 * hashCode + ((this.name == null) ? 0 : this.description.hashCode());
+        return hashCode;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if ((obj == null) || (!(obj instanceof Sport)))
+            return false;
+
+        if (this == obj)
+            return true;
+
+        Sport objSport = (Sport) obj;
+        if (name == null) {
+            if (objSport.getName() != null)
+                return false;
+        } else if (!name.equals(objSport.getName())) {
+            return false;
+        }
+
+        if (description == null) {
+            if (objSport.getDescription() != null)
+                return false;
+        } else if (!description.equals(objSport.getDescription()))
+            return false;
+
+        return true;
+    }
+}
