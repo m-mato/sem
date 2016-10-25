b5973d065222966d9f5fa5647fb14cb8b25726f1
Matej Majdis <mato.majdis@gmail.com>
2016-10-25 17:10:57 +0200

    Milestone 1 : DB fix

--- src/main/java/cz/muni/pa165/sem/configuration/DBConfig.java
+++ src/main/java/cz/muni/pa165/sem/configuration/DBConfig.java
@@ -1,10 +1,22 @@
 package cz.muni.pa165.sem.configuration;
 
+import org.hibernate.jpa.HibernatePersistenceProvider;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.instrument.classloading.InstrumentationLoadTimeWeaver;
+import org.springframework.instrument.classloading.LoadTimeWeaver;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 import javax.sql.DataSource;
 
@@ -13,11 +25,53 @@ import javax.sql.DataSource;
  * @author Matej Majdis
  */
 @Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories
+@ComponentScan(basePackages = "cz.muni.pa165.sem")
 public class DBConfig {
 
 	@Bean
-	public DataSource dataSource() {
+	public JpaTransactionManager transactionManager() {
+		return new JpaTransactionManager(entityManagerFactory().getObject());
+	}
 
+	/**
+	 * Starts up a container that emulates behavior prescribed in JPA spec for container-managed EntityManager
+	 *
+	 * @return entity manager factory to be used
+	 */
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+		LocalContainerEntityManagerFactoryBean jpaFactoryBean = new LocalContainerEntityManagerFactoryBean();
+		jpaFactoryBean.setDataSource(db());
+		jpaFactoryBean.setJpaVendorAdapter(jpaVendorAdapter());
+		jpaFactoryBean.setLoadTimeWeaver(instrumentationLoadTimeWeaver());
+		jpaFactoryBean.setPersistenceXmlLocation("classpath:META-INF/persistence.xml");
+		jpaFactoryBean.setPersistenceProviderClass(HibernatePersistenceProvider.class);
+		return jpaFactoryBean;
+	}
+
+	@Bean
+	public JpaVendorAdapter jpaVendorAdapter() {
+		HibernateJpaVendorAdapter hibernateJpaVendorAdapter = new HibernateJpaVendorAdapter();
+		hibernateJpaVendorAdapter.setShowSql(true);
+		hibernateJpaVendorAdapter.setGenerateDdl(true);
+		hibernateJpaVendorAdapter.setDatabase(Database.DERBY);
+		return hibernateJpaVendorAdapter;
+	}
+
+	@Bean
+	public LocalValidatorFactoryBean localValidatorFactoryBean() {
+		return new LocalValidatorFactoryBean();
+	}
+
+	@Bean
+	public LoadTimeWeaver instrumentationLoadTimeWeaver() {
+		return new InstrumentationLoadTimeWeaver();
+	}
+
+	@Bean
+	public DataSource db() {
 		EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
 		EmbeddedDatabase db = builder
 				.setType(EmbeddedDatabaseType.DERBY)
@@ -28,3 +82,4 @@ public class DBConfig {
 		return db;
 	}
 }
+
