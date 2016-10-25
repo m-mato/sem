b87c562fcc97b4fff25d53e3a5489806f5978b68
Matej Majdis <mato.majdis@gmail.com>
2016-10-25 16:51:41 +0200

    Milestone 1 : Repository -> DAO - basic methods implemented for SportsmanDAOImpl

--- src/main/java/cz/muni/pa165/sem/dao/SportsmanDAOImpl.java
+++ src/main/java/cz/muni/pa165/sem/dao/SportsmanDAOImpl.java
@@ -1,7 +1,30 @@
 package cz.muni.pa165.sem.dao;
 
+import cz.muni.pa165.sem.entity.Sportsman;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import java.util.List;
+
 /**
  * @author Matej Majdis
  */
+@Repository
 public class SportsmanDAOImpl implements SportsmanDAO {
+
+	@Autowired
+	private EntityManager entityManager;
+
+	public void addSportsman(Sportsman sportsman) {
+
+		entityManager.persist(sportsman);
+	}
+
+	public List<Sportsman> getAll() {
+
+		Query query = entityManager.createQuery("SELECT s FROM Sportsman s");
+		return  query.getResultList();
+	}
 }
