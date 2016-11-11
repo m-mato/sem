5f605e73e4b59eb528d29df0046d387eee0f7a52
Vit Hovezak <kybersoft@gmail.com>
2016-11-11 17:37:20 +0100

    Add basic EventService and EventServiceImpl

---
+++ sem-service/src/main/java/cz/muni/pa165/sem/service/EventServiceImpl.java
@@ -0,0 +1,76 @@
+package cz.muni.pa165.sem.service;
+
+import cz.muni.pa165.sem.dao.EventDAO;
+import cz.muni.pa165.sem.entity.Event;
+import cz.muni.pa165.sem.entity.Sport;
+import cz.muni.pa165.sem.entity.Sportsman;
+import java.util.Calendar;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Vit Hovezak
+ */
+@Service
+public class EventServiceImpl implements EventService {
+
+    @Autowired
+    private EventDAO eventDAO;
+
+    @Override
+    public void create(Event event) {
+        eventDAO.create(event);
+    }
+
+    @Override
+    public Event findById(Long id) {
+        return eventDAO.findById(id);
+    }
+
+    @Override
+    public List<Event> findByName(String name) {
+        return eventDAO.findByName(name);
+    }
+
+    @Override
+    public List<Event> findByDate(Calendar date) {
+        return eventDAO.findByDate(date);
+    }
+
+    @Override
+    public List<Event> findBySport(Sport sport) {
+        return eventDAO.findBySport(sport);
+    }
+
+    @Override
+    public List<Event> findByCity(String city) {
+        return eventDAO.findByCity(city);
+    }
+
+    @Override
+    public List<Event> findByAdmin(Sportsman admin) {
+        return eventDAO.findByAdmin(admin);
+    }
+
+    @Override
+    public List<Event> findByParticipant(Sportsman participant) {
+        return eventDAO.findByParticipant(participant);
+    }
+
+    @Override
+    public List<Event> findAll() {
+        return eventDAO.findAll();
+    }
+
+    @Override
+    public void update(Event event) {
+        eventDAO.update(event);
+    }
+
+    @Override
+    public void delete(Event event) {
+        eventDAO.delete(event);
+    }
+
+}
