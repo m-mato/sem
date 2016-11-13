1bd990d4dccda1fe001d98dd1953eb91f6094fbf
Vit Hovezak <kybersoft@gmail.com>
2016-11-13 16:57:05 +0100

    Add exceptions for EventServiceImpl

--- sem-service/src/main/java/cz/muni/pa165/sem/service/EventServiceImpl.java
+++ sem-service/src/main/java/cz/muni/pa165/sem/service/EventServiceImpl.java
@@ -7,6 +7,8 @@ import cz.muni.pa165.sem.entity.Sportsman;
 import java.util.Calendar;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.DataRetrievalFailureException;
 import org.springframework.stereotype.Service;
 
 /**
@@ -20,57 +22,101 @@ public class EventServiceImpl implements EventService {
 
     @Override
     public void create(Event event) {
-        eventDAO.create(event);
+        try {
+            eventDAO.create(event);
+        } catch (Exception e) {
+            throw new DataIntegrityViolationException("Failed to create " + event + ", exception: ", e);
+        }
     }
 
     @Override
     public Event findById(Long id) {
-        return eventDAO.findById(id);
+        try {
+            return eventDAO.findById(id);
+        } catch (Exception e) {
+            throw new DataRetrievalFailureException("Failed to find event by id " + id + ", exception: ", e);
+        }
     }
 
     @Override
     public List<Event> findByName(String name) {
-        return eventDAO.findByName(name);
+        try {
+            return eventDAO.findByName(name);
+        } catch (Exception e) {
+            throw new DataRetrievalFailureException("Failed to find event by name " + name + ", exception: ", e);
+        }
     }
 
     @Override
     public List<Event> findByDate(Calendar date) {
-        return eventDAO.findByDate(date);
+        try {
+            return eventDAO.findByDate(date);
+        } catch (Exception e) {
+            throw new DataRetrievalFailureException("Failed to find event by date " + date + ", exception: ", e);
+        }
     }
 
     @Override
     public List<Event> findBySport(Sport sport) {
-        return eventDAO.findBySport(sport);
+        try {
+            return eventDAO.findBySport(sport);
+        } catch (Exception e) {
+            throw new DataRetrievalFailureException("Failed to find event by sport " + sport + ", exception: ", e);
+        }
     }
 
     @Override
     public List<Event> findByCity(String city) {
-        return eventDAO.findByCity(city);
+        try {
+            return eventDAO.findByCity(city);
+        } catch (Exception e) {
+            throw new DataRetrievalFailureException("Failed to find event by city " + city + ", exception: ", e);
+        }
     }
 
     @Override
     public List<Event> findByAdmin(Sportsman admin) {
-        return eventDAO.findByAdmin(admin);
+        try {
+            return eventDAO.findByAdmin(admin);
+        } catch (Exception e) {
+            throw new DataRetrievalFailureException("Failed to find event by admin " + admin + ", exception: ", e);
+        }
     }
 
     @Override
     public List<Event> findByParticipant(Sportsman participant) {
-        return eventDAO.findByParticipant(participant);
+        try {
+            return eventDAO.findByParticipant(participant);
+        } catch (Exception e) {
+            throw new DataRetrievalFailureException("Failed to find event by participant " + participant + ", exception: ", e);
+        }
     }
 
     @Override
     public List<Event> findAll() {
-        return eventDAO.findAll();
+        try {
+            return eventDAO.findAll();
+        } catch (Exception e) {
+            throw new DataRetrievalFailureException("Failed to find all events, exception: ", e);
+        }
     }
 
     @Override
     public void update(Event event) {
-        eventDAO.update(event);
+        try {
+            eventDAO.update(event);
+        } catch (Exception e) {
+            throw new DataIntegrityViolationException("Failed to update " + event + ", exception: ", e);
+        }
     }
 
     @Override
     public void delete(Event event) {
-        eventDAO.delete(event);
+        try {
+            eventDAO.delete(event);
+        } catch (Exception e) {
+            throw new DataIntegrityViolationException("Failed to delete " + event + ", exception: ", e);
+        }
     }
 
 }
