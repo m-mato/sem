ad0c3db03539783fd096480a56165fc2ac7293b8
Vit Hovezak <kybersoft@gmail.com>
2016-11-25 15:47:54 +0100

    Add SportFacadeTest for service module

--- sem-service/src/test/java/cz.muni.pa165.sem/facade/SportFacadeTest.java
+++ sem-service/src/test/java/cz.muni.pa165.sem/facade/SportFacadeTest.java
@@ -1,7 +1,135 @@
 package cz.muni.pa165.sem.facade;
 
+import cz.muni.pa165.sem.dto.SportCreateDTO;
+import cz.muni.pa165.sem.dto.SportDTO;
+import cz.muni.pa165.sem.dto.SportUpdateDTO;
+import cz.muni.pa165.sem.entity.Sport;
+import cz.muni.pa165.sem.service.BeanMappingService;
+import cz.muni.pa165.sem.service.SportService;
+import cz.muni.pa165.sem.service.facade.SportFacadeImpl;
+
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.not;
+import static org.mockito.Matchers.argThat;
+
 /**
- * Created by Kamil on 21-Nov-16.
+ * @author Vit Hovezak
  */
-public class SportFacadeTest {
+@ContextConfiguration(locations = "classpath:service-test-context.xml")
+@RunWith(MockitoJUnitRunner.class)
+public class SportFacadeTest extends AbstractTestNGSpringContextTests {
+
+    @InjectMocks
+    private SportFacadeImpl sportFacade = new SportFacadeImpl();
+
+    @Mock
+    private SportService sportService;
+
+    @Autowired
+    private BeanMappingService beanMappingService;
+
+    private Sport sport;
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        sportFacade.setBeanMappingService(beanMappingService);
+
+        sport = new Sport();
+        sport.setName("Sport 1 name");
+        sport.setDescription("Sport 1 description");
+
+        Mockito.when(sportService.findById(1L)).thenReturn(sport);
+        Mockito.when(sportService.findById(argThat(not(1L)))).thenReturn(null);
+        Mockito.when(sportService.findByName("Sport 1 name")).thenReturn(sport);
+        Mockito.when(sportService.findByName(argThat(not("Sport 1 name")))).thenReturn(null);
+        Mockito.when(sportService.findAll()).thenReturn(Collections.singletonList(sport));
+    }
+
+    @Test
+    public void create() {
+        SportCreateDTO sportCreateDTO = new SportCreateDTO();
+        sportCreateDTO.setName("Sport 2 name");
+        sportCreateDTO.setDescription("Sport 2 description");
+
+        sportFacade.create(sportCreateDTO);
+        Sport createdSport = beanMappingService.mapTo(sportCreateDTO, Sport.class);
+
+        Mockito.verify(sportService).create(createdSport);
+    }
+
+    @Test
+    public void getExistingSportById() {
+        sportFacade.getSportById(1L);
+
+        Mockito.verify(sportService).findById(1L);
+    }
+
+    @Test
+    public void getNonExistingSportById() {
+        SportDTO sportDTO = sportFacade.getSportById(2L);
+
+        Mockito.verify(sportService).findById(2L);
+        Assert.assertNull(sportDTO);
+    }
+
+    @Test
+    public void getExistingSportByName() {
+        sportFacade.getSportByName("Sport 1 name");
+
+        Mockito.verify(sportService).findByName("Sport 1 name");
+    }
+
+    @Test
+    public void getNonExistingSportByName() {
+        SportDTO sportDTO = sportFacade.getSportByName("Sport 2 name");
+
+        Mockito.verify(sportService).findByName("Sport 2 name");
+        Assert.assertNull(sportDTO);
+    }
+
+    @Test
+    public void getAllSports() {
+        List<SportDTO> sports = sportFacade.getAllSports();
+
+        Mockito.verify(sportService).findAll();
+        Assert.assertNotNull(sports);
+        Assert.assertEquals(sports.size(), 1);
+    }
+
+    @Test
+    public void update() {
+        SportUpdateDTO sportUpdateDTO = beanMappingService.mapTo(sport, SportUpdateDTO.class);
+        sportUpdateDTO.setId(1L);
+
+        sportFacade.update(sportUpdateDTO);
+
+        Mockito.verify(sportService).update(sport);
+    }
+
+    @Test
+    public void delete() {
+        SportDTO sportDTO = sportFacade.getSportById(1L);
+        sportDTO.setId(1L);
+
+        sportFacade.delete(sportDTO.getId());
+
+        Mockito.verify(sportService).delete(sport);
+    }
+
 }
