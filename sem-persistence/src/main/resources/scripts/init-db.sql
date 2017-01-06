c00cd2e27d68f03154e713415bb816cbef3b9c19
veronikaaksamit <veronika.aksamitova@gmail.com>
2017-01-06 18:58:07 +0100

    Added some data to init-db

--- sem-persistence/src/main/resources/scripts/init-db.sql
+++ sem-persistence/src/main/resources/scripts/init-db.sql
@@ -7,19 +7,29 @@ INSERT INTO sportsman(name, surname, birth_date, email, password, is_manager) VA
 --$2a$06$ZLWy4mNTyNWVJ5h7nVsWzut9G3mtIGI05uxGGne22Tx/RFvYjNhb2 = supersilneheslo
 
 INSERT INTO sport(name, description) VALUES ('Triathlon', 'Triathlon description');
+INSERT INTO sport(name, description) VALUES ('Polo', 'Water sport with ball');
+INSERT INTO sport(name, description) VALUES ('Tennis', 'Tennis description');
+INSERT INTO sport(name, description) VALUES ('Baseball', 'Baseball description');
 INSERT INTO sport(name, description) VALUES ('Swimming', 'Swimming description');
 
-INSERT INTO event(name, description, event_date, sport, capacity, city, address, admin) VALUES ('Triathlon Brno', 'Triathlon in Brno', '2016-08-30 19:05:00', 1, 100, 'Brno', 'Brno, 60200 - CR', 1);
-INSERT INTO event(name, description, event_date, sport, capacity, city, address, admin) VALUES ('Triathlon Brno2', 'Triathlon in Brno', '2016-09-30 19:05:00', 1, 100, 'Brno', 'Brno, 60200 - CR', 2);
-INSERT INTO event(name, description, event_date, sport, capacity, city, address, admin) VALUES ('Triathlon Brno3', 'Triathlon in Brno', '2016-10-30 19:05:00', 1, 100, 'Brno', 'Brno, 60200 - CR', 2);
+INSERT INTO event(name, description, event_date, sport, capacity, city, address, admin) VALUES ('Triathlon Brno', 'Triathlon in Brno', '2017-08-30 19:05:00', 1, 100, 'Brno', 'Brno, 60200 - CR', 1);
+INSERT INTO event(name, description, event_date, sport, capacity, city, address, admin) VALUES ('Triathlon Brno2', 'Triathlon in Brno', '2017-09-30 19:05:00', 1, 100, 'Brno', 'Brno, 60200 - CR', 2);
+INSERT INTO event(name, description, event_date, sport, capacity, city, address, admin) VALUES ('Triathlon Brno3', 'Triathlon in Brno', '2017-10-30 19:05:00', 1, 100, 'Brno', 'Brno, 60200 - CR', 2);
+INSERT INTO event(name, description, event_date, sport, capacity, city, address, admin) VALUES ('Tennis 1 Brno', 'Tennis in Brno', '2017-10-28 18:05:00', 3, 80, 'Brno', 'Brno, 60200 - CR', 1);
+INSERT INTO event(name, description, event_date, sport, capacity, city, address, admin) VALUES ('Baseball 1 Brno', 'Fresh new event', '2017-10-29 17:05:00', 4, 70, 'Brno', 'Brno, 60200 - CR', 2);
+INSERT INTO event(name, description, event_date, sport, capacity, city, address, admin) VALUES ('Tennis 2 Brno', 'Tennis in Brno for the second time', '2017-10-29 18:05:00', 3, 80, 'Brno', 'Brno, 60200 - CR', 1);
+INSERT INTO event(name, description, event_date, sport, capacity, city, address, admin) VALUES ('Baseball 2 Brno', 'New event for baseball fans', '2017-10-30 17:05:00', 4, 70, 'Brno', 'Brno, 60200 - CR', 2);
+INSERT INTO event(name, description, event_date, sport, capacity, city, address, admin) VALUES ('Swimming Brno', 'New event for fans of swimming', '2017-11-29 17:05:00', 5, 70, 'Brno', 'Brno, 60200 - CR', 2);
 
 INSERT INTO result(performance,  performance_unit, position, sportsman, note, event) VALUES (20220.2, 1, 1, 1, 'Winner', 1);
-INSERT INTO result(performance,  performance_unit, position, sportsman, note, event) VALUES (5456.2, 1, 2, 1, 'silver medal', 2);
-INSERT INTO result(performance,  performance_unit, position, sportsman, note, event) VALUES (6.2, 4, 1, 6, 'gold medal', 2);
-INSERT INTO result(performance,  performance_unit, position, sportsman, note, event) VALUES (7.2, 4, 1, 6, 'gold', 3);
-INSERT INTO result(performance,  performance_unit, position, sportsman, note, event) VALUES (6.2, 4, 1, 5, 'gold medal', 1);
-INSERT INTO result(performance,  performance_unit, position, sportsman, note, event) VALUES (7, 3, 3, 6, 'third place', 3);
+INSERT INTO result(performance,  performance_unit, position, sportsman, note, event) VALUES (20220.5, 1, 2, 6, 'silver medal', 1);
+INSERT INTO result(performance,  performance_unit, position, sportsman, note, event) VALUES (20220.2, 1, 1, 5, 'gold medal', 2);
+INSERT INTO result(performance,  performance_unit, position, sportsman, note, event) VALUES (54256.2, 1, 2, 1, 'silver medal', 2);
+INSERT INTO result(performance,  performance_unit, position, sportsman, note, event) VALUES (12, 5, 1, 3, 'gold', 4);
+INSERT INTO result(performance,  performance_unit, position, sportsman, note, event) VALUES (7, 2, 3, 4, 'third place', 8);
 
-INSERT INTO invitation(event, invitee, state) VALUES(1, 6, 4);
-INSERT INTO invitation(event, invitee, state) VALUES(2, 6, 3);
-INSERT INTO invitation(event, invitee, state) VALUES(3, 6, 1);
+INSERT INTO invitation(event, invitee, state) VALUES(4, 6, 1);
+INSERT INTO invitation(event, invitee, state) VALUES(5, 6, 2);
+INSERT INTO invitation(event, invitee, state) VALUES(6, 6, 3);
+INSERT INTO invitation(event, invitee, state) VALUES(7, 6, 4);
+INSERT INTO invitation(event, invitee, state) VALUES(8, 6, 5);
